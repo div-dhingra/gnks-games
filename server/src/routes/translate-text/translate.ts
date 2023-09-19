7a21957cbc9682bea873d56fcb6c46e2ccc57460
Divpreet Dhingra <divdhingra@Divpreets-Laptop.local>
2023-09-19 05:18:34 -0700

    Test post requests

--- server/src/routes/translate-text/translate.ts
+++ server/src/routes/translate-text/translate.ts
@@ -4,53 +4,55 @@ const router_Translate : Router = expressClass.Router();
 
 router_Translate.post('/', async (req : Request, res : Response) => {
 
-    const url : string = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
-    const textToTranslate : string = req.body.punjabiTextToTranslate;
+    // const url : string = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+    // const textToTranslate : string = req.body.punjabiTextToTranslate;
    
-    const options = {
+    // const options = {
 
-        method: 'POST',
-        headers: {
+    //     method: 'POST',
+    //     headers: {
 
-            'content-type': 'application/x-www-form-urlencoded',
+    //         'content-type': 'application/x-www-form-urlencoded',
 
-            'Accept-Encoding': 'application/gzip',
+    //         'Accept-Encoding': 'application/gzip',
 
-            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+    //         'X-RapidAPI-Key': process.env.RAPID_API_KEY,
 
-            'X-RapidAPI-Host': process.env.RAPID_API_HOST
-        },
-
-
+    //         'X-RapidAPI-Host': process.env.RAPID_API_HOST
+    //     },
 
 
 
-        body: new URLSearchParams({
-            q: textToTranslate, 
-            target: 'en',
-            source: 'pa'
-        })
 
-    };
 
-    try {
+    //     body: new URLSearchParams({
+    //         q: textToTranslate, 
+    //         target: 'en',
+    //         source: 'pa'
+    //     })
 
-        const response = await fetch(url, options);
+    // };
 
-        if (!response.ok) {
-            throw new Error(`HTTP-status Error: ${response.status}`);
-        } 
+    // try {
 
-        const result = await response.json();
-        const translatedText = await result.data.translations[0].translatedText;
+    //     const response = await fetch(url, options);
 
-        console.log(translatedText);
+    //     if (!response.ok) {
+    //         throw new Error(`HTTP-status Error: ${response.status}`);
+    //     } 
 
-        res.send(translatedText); 
+    //     const result = await response.json();
+    //     const translatedText = await result.data.translations[0].translatedText;
+
+    //     console.log(translatedText);
+
+    //     res.send(translatedText); 
                 
-    } catch (error) {
-	    console.error(error);
-    }
+    // } catch (error) {
+	//     console.error(error);
+    // }
+
+    res.send({"Test" : "Post goes through"});
    
 })
 
