56d62665adb5b087360bfb9f22bcde0716e70eaa
Divpreet Dhingra <divdhingra@Divpreets-Laptop.local>
2023-09-19 23:58:11 -0700

    Add react-router comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,15 @@ import Home from './WebsiteComponents/belowNavBarContent/Home/Home';
 import About from './WebsiteComponents/belowNavBarContent/About/About';
 import Hangman from './WebsiteComponents/belowNavBarContent/Games/Hangman/Hangman';
 
+// * NOTE: To use 'react-router' (client-side routing) in my React-Application (CRA, Vite, etc.) in render.com,
+// * I need to add a 'redirect' rule to allow continuous refreshing and retainence of the subpage (/PunjabiHangman, /About)
+// * INSTEAD OF ending up with my subpages (react-router-dom client-side-routed subpages) showing 'NOT FOUND' 
+// * on 1 refresh...
+
+// * SOURCE: 
+// * https://community.render.com/t/refreshing-production-subpage-does-not-work/12260
+// * https://render.com/docs/deploy-create-react-app#using-client-side-routing
+
 const App = () => {
 
   const [levelSelected, setLevelSelected] = useState<number>(0);
