d1cf8cd6452cea8cb625b4791b8d0f2e3b1d7584
Divpreet Dhingra <divdhingra@Divpreets-Laptop.local>
2023-09-20 01:03:43 -0700

    Removed react strictmode behavior in production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,13 +9,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   
-  <React.StrictMode>
-
     <BrowserRouter>
       <App />
     </BrowserRouter> 
-    
-  </React.StrictMode>
   
 );
 
